6cdbe75e3f5c58456ca0d9ee689a06344ce4f8f9
pmayur1998 <mayurpancholi25@gmail.com>
2025-04-03 17:46:38 +0530

    feat: name change

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TestAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "new",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
